0dad5d50b4152b87099be6a21e9979a3f1914746
VasudeepNarra <VasudeepNarra@missionrnd.com>
2016-03-16 16:27:12 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -11,80 +11,33 @@ NOTES: Don't create new string.
 */
 #include <Stdio.h>
 #include <string.h>
-
-void str_words_in_rev(char *input, int len)
+void reverse(char *q, char *r)
 {
-	int i = 0, j = 0;
-	char arr[100][100];
-	while (*input != '\0')
+	char s;
+	while (q < r)
 	{
-		if (*input == ' ')
-		{
-			arr[i][j] = ' ';
-			*input++;
-			i++;
-			j = 0;
-		}
-		else
-		{
-			arr[i][j] = *input;
-			j++; *input++;
-		}
-	}
-	j = 0; int k = i;
-	while (k >= 0)
-	{
-		if (arr[k][j] == ' ')
-		{
-			*input = arr[k][j];
-			k--; j = 0;
-		}
-		*input = arr[k][j];
-		j++; *input++;
+		s = *q;
+		*q++ = *r;
+		*r-- = s;
 	}
 }
-
-
-
-
-
-	/*
-	int space = 0;
-	char *temp, *temp2 = NULL, *temp3;
-	int l = strlen(input), i = 0, j = 0;
-	while (*input != '\0')
+void str_words_in_rev(char *input, int len)
+{
+	char *p = input;
+	char *t = input;
+	while (*t)
 	{
-		if (*input == ' ') space++;
-		*input++;
-	}
-
-	while (space != 0)
-	{
-		temp = input;
-		while (*input != ' '&&*input != '\0')
+		t++;
+		if (*t == '\0')
 		{
-			temp2[i++] = *input;
-			*input++;
+			reverse(p, t - 1);
 		}
-		if (*input == '\0') break;
-		if (*(input + 1) != '\0')
+		else if (*t == ' ')
 		{
-			*input++;
-			while (*(input + 1) != '\0' && input != temp3)
-			{
-				*(input - i - 1) = *input;
-				*input++;
-			}
-			input = (input - i);
-			temp3 = input; *input = ' '; *input++;
-			while (*temp2 != '\0')
-			{
-				*input = *temp2;
-				*input++; *temp2++;
-			}
+			reverse(p, t - 1);
+			p = t + 1;
 		}
-		space--;
 	}
-	*/
-	
+	reverse(input, t - 1);
+}
 
